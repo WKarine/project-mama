fbcbf8a16594bc348c5e4f26b568794d49e537aa
Gutsover <mylenevandaele@hmail.com>
2018-04-11 16:34:32 +0100

    Add 2017-en-chiffre new page

---
+++ pages/2017-en-chiffres.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+import Categorie from "../components/Categorie";
+
+export default () => (
+  <Layout>
+    <Categorie name="2017-en-chiffres" />
+  </Layout>
+);
