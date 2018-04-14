e18d24dfdb724c7cc4aceb1ea021452dce5f73c9
Thomas Ringot <contact@thomasringot.com>
2018-04-14 14:04:38 +0200

    Add logo in sidebar

--- components/Layout/Sidebar/index.js
+++ components/Layout/Sidebar/index.js
@@ -1,4 +1,5 @@
 import CategorieLink from './CategorieLink';
+import Link from 'next/link';
 
 class Sidebar extends React.Component {
   componentDidMount() {
@@ -10,12 +11,23 @@ class Sidebar extends React.Component {
     return (
       <div>
         <ul id="slide-out" className="sidenav sidenav-fixed">
+
+          <Link href="/">
+            <a><img className="logo center-align" src="static/images/logo.png" /></a>
+          </Link>
           {
             this.props.links.map(link => <CategorieLink {...link} key={link.textContent} />)
           }
         </ul>
         <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
         <style jsx>{`
+          .logo {
+            display: block;
+            max-width: 13rem;
+            margin: auto;
+            padding-top: 2rem;
+            padding-bottom: 2rem;
+          }
           .sidenav-trigger {
             position: absolute;
             top: 1rem;
