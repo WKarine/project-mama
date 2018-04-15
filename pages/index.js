71aecd5d7199d14bda7b5293ec9aaba118484e89
Gutsover <mylenevandaele@hmail.com>
2018-04-15 15:42:05 +0100

    Add h1 index css

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,33 @@ import Category from "../components/Category";
 
 export default () => (
   <Layout>
-    <h1>Test</h1>
+    <h1 className="principal-title">rapport annuel d'activités</h1>
+
+    <style jsx>
+      {`
+        .principal-title {
+          font-size: 4rem;
+          font-variant: small-caps;
+        }
+
+        .principal-title:after {
+          width: 12rem;
+          height: 0.8rem;
+          content: "";
+          display: inline-block;
+          position: relative;
+          background-color: grey;
+        }
+
+        .principal-title:before {
+          width: 12rem;
+          height: 0.8rem;
+          content: "";
+          display: inline-block;
+          position: relative;
+          background-color: grey;
+        }
+      `}
+    </style>
   </Layout>
 );
