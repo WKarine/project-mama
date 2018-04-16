29f1eb5175551f9ee0a7879d6f25abf5ca2c659c
Thomas Ringot <contact@thomasringot.com>
2018-04-16 14:29:46 +0200

    Update Example

--- pages/example.js
+++ pages/example.js
@@ -1,19 +1,19 @@
 import Layout from "../components/Layout";
 
-// import Card from "../components/Card";
-// import Chart from "../components/Chart";
+import ImageCard from "../components/ImageCard";
 
 export default () => (
   <Layout>
     {/* row obligatoire, vous pouvez en ajouter d'autre ensuite si besoin */}
     <div className="row">
       {/* ensuite a vous de choisir ce que vous mettez, pas obligé un col 12 */}
-      <div className="col s12">
+      <div className="col s12 m5">
         {/* ici le contenu */}
         <h1>Ceci est un titre</h1>
-        <Card className="special-card" />
-        <h2>Sous-titre</h2>
-        <Chart type="pie" />
+
+        <ImageCard imageSrc="static/images/events/4.png" title="prout">
+          <p>Coucou</p>
+        </ImageCard>
       </div>
     </div>
 
