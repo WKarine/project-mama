412cd2cc8fe07b21a14f52d1fb394ad7f382089c
Thomas Ringot <contact@thomasringot.com>
2018-04-16 07:36:18 +0200

    Refactorise Layout Component

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,33 +1,32 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import Sidenav from './Sidenav'
+import Sidenav from "./Sidenav";
 
 import { categoryLinks } from "../../models/components/Sidenav";
 
-const Layout = (props) => (
+const Layout = ({ children }) => (
   <div className="container">
     <Head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      />
       <link rel="stylesheet" href="static/vendors/materialize.min.css" />
-
       <link rel="stylesheet" href="static/css/index.css" />
+
       <title>Project-Mama</title>
     </Head>
     <React.Fragment>
       <Sidenav categoryLinks={categoryLinks} />
 
-      <main className="content">
-        {
-          props.children
-        }
-      </main>
+      <main className="content">{children}</main>
     </React.Fragment>
 
-    <script src="static/vendors/materialize.min.js"></script>
+    <script src="static/vendors/materialize.min.js" />
   </div>
 );
 
-export default Layout
+export default Layout;
