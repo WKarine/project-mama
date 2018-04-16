c50f79f2b0241cc22e908d8621eece155c9ff270
WKarine <karine.wloch@gmail.com>
2018-04-16 15:58:44 +0200

    Add new sidebar

--- models/components/Sidenav/index.js
+++ models/components/Sidenav/index.js
@@ -1,31 +1,126 @@
 const categoryLinks = [
   {
-    textContent: "perspectives",
+    textContent: "Perspectives",
     href: "perspectives"
   },
-  {
-    textContent: "fiche d'identité",
-    href: "fiche-identite"
-  },
-  {
-    textContent: "cartographie",
-    href: "cartographie"
-  },
-  {
-    textContent: "santé des salariés",
-    href: "sante-des-salaries"
-  },
   [
-    "sous menu",
+    "Fiche d'identité",
     {
-      textContent: "test",
-      href: "test"
+      textContent: "Qui sommes-nous",
+      href: "qui-sommes-nous"
     },
     {
-      textContent: "autre test",
-      href: "autre-test"
+      textContent: "Evénements de l'année",
+      href: "evenement-annees"
+    },
+    {
+      textContent: "Chiffres clés",
+      href: "chiffres cles"
     }
-  ]
+  ],
+  [
+    "Cartographie des entreprises",
+    {
+      textContent: "Un territoire contrasté",
+      href: "territoire-contraste"
+    },
+    {
+      textContent: "Le profil des entreprises",
+      href: "profil entreprises"
+    },
+    {
+      textContent: "L'état de santé des salariés",
+      href: "etat-de-sante"
+    }
+  ],
+  [
+    "Veiller sur la santé",
+    {
+      textContent: "Un suivi de santé adapté",
+      href: "suivi-de-sante"
+    },
+    {
+      textContent: "Le parcours de santé",
+      href: "parcours-de-sante"
+    },
+    {
+      textContent: "Un travail d'équipe",
+      href: "sante-travail-equipe"
+    },
+    {
+      textContent: "La traçabilité",
+      href: "tracabilite"
+    },
+    {
+      textContent: "Le suivi en chiffres",
+      href: "suivi-en-chiffres"
+    },
+  ],
+  [
+    "Ecouter et accompagner ",
+    {
+      textContent: "Evaluation des risques",
+      href: "aide-evaluation-risques"
+    },
+    {
+      textContent: "Conseils en entreprise",
+      href: "conseils-en-entreprise"
+    },
+    {
+      textContent: "Actions en milieu de travail",
+      href: "action-en-milieu-de-travail"
+    },
+    {
+      textContent: "Enquêtes et diagnostics",
+      href: "enquetes-diagnostic"
+    },
+    {
+      textContent: "Drogues et alcool",
+      href: "drogues-alcool"
+    },
+    {
+      textContent: "Maintien dans l'emploi",
+      href: "maintien-emploi"
+    },
+    {
+      textContent: "Formation des salariés",
+      href: "formation-des-salaries"
+    },
+  ],
+  [
+    "Mener des actions",
+    {
+      textContent: "CHRD",
+      href: "chrd"
+    },
+    {
+      textContent: "Colorisk",
+      href: "colorisk"
+    },
+    {
+      textContent: "Métiers du bois",
+      href: "metiers-du-bois"
+    }
+  ],
+  [
+    "Mettre des moyens",
+    {
+      textContent: "Relations adhérents",
+      href: "relations-adherents"
+    },
+    {
+      textContent: "Communication",
+      href: "communication"
+    },
+    {
+      textContent: "Immobiliers investissements",
+      href: "immobiliers-investissements"
+    }
+  ],
+  [
+    "Bilan financier",
+  ],
+
 ];
 
 export { categoryLinks };
