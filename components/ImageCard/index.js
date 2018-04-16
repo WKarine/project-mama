032a2181ba3e1871adc94b29bb300ab3df3f0658
Thomas Ringot <contact@thomasringot.com>
2018-04-16 13:24:12 +0200

    Refactor ImageCard component

--- components/ImageCard/index.js
+++ components/ImageCard/index.js
@@ -1,24 +1,22 @@
-const ImageCard = ({ imageSrc, title, children }) => {
-  return (
-    <div className="card">
-      <div className="card-image waves-effect waves-block waves-light">
-        <img className="activator" src={imageSrc} />
-      </div>
-      <div className="card-content">
-        <span className="card-title activator grey-text text-darken-4">
-          {title}
-          <i className="material-icons right">more_vert</i>
-        </span>
-      </div>
-      <div className="card-reveal">
-        <span className="card-title grey-text text-darken-4">
-          {title}
-          <i className="material-icons right">close</i>
-        </span>
-        {children}
-      </div>
+const ImageCard = ({ imageSrc, title, children }) => (
+  <div className="card">
+    <div className="card-image waves-effect waves-block waves-light">
+      <img className="activator" src={imageSrc} />
     </div>
-  );
-};
+    <div className="card-content">
+      <span className="card-title activator grey-text text-darken-4">
+        {title}
+        <i className="material-icons right">more_vert</i>
+      </span>
+    </div>
+    <div className="card-reveal">
+      <span className="card-title grey-text text-darken-4">
+        {title}
+        <i className="material-icons right">close</i>
+      </span>
+      {children}
+    </div>
+  </div>
+);
 
 export default ImageCard;
