33ce51f2f4e13390984ec5d28934796fc8ae6c26
Thomas Ringot <contact@thomasringot.com>
2018-04-16 07:32:14 +0200

    Simplify Card Component

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,30 +1,11 @@
-import { categoryColors } from "../../constantes/colors";
-
-const Card = props => {
-  const textClass = props.textColor === "white" ? "white-text" : "black-text";
-  const cardClass = "card-panel " + props.cardColor;
-
-  return (
-    <div className="col s12 m4">
-      <div className={cardClass}>
-        {props.h4 && <h4>{props.h4}</h4>}
-
-        <span className={textColor}>{props.content}</span>
-
-        <span>
-          <ul>{props.ul.map(liContent => <li>{liContent}</li>)}</ul>
-        </span>
-      </div>
-      <style jsx>
-        {`
-          .blue {
-            background-color: ${categoryColors.blue};
-            color: white;
-          }
-        `}
-      </style>
-    </div>
-  );
-};
+const Card = () => (
+  <div className="card-panel teal">
+    <span className="white-text">
+      I am a very simple card. I am good at containing small bits of
+      information. I am convenient because I require little markup to use
+      effectively. I am similar to what is called a panel in other frameworks.
+    </span>
+  </div>
+);
 
 export default Card;
