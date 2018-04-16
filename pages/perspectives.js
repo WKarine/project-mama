e46343f9d3f6d1829e89bd70dacccf384e48a3e3
Gutsover <mylenevandaele@hmail.com>
2018-04-16 11:35:22 +0100

    Add imagecard + test in perspectives page

--- pages/perspectives.js
+++ pages/perspectives.js
@@ -1,7 +1,24 @@
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
+import H2 from "../components/H2";
+import ListCard from "../components/ListCard";
+import ImageCard from "../components/ImageCard";
 
 export default () => (
   <Layout>
-    <h1>Perspectives</h1>
+    <div className="row">
+      <div className="col s12">
+        <h1>Perspectives</h1>
+      </div>
+
+      <div className="col s12">
+        <H2>titre h2</H2>
+      </div>
+
+      <div className="col s12 m4">
+        <ImageCard title="Event 1" imageSrc="../static/images/events/4.png ">
+          <p>DEDEDE</p>
+        </ImageCard>
+      </div>
+    </div>
   </Layout>
-)
+);
