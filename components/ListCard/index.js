d0ea89c1ea9c7b12861892393a4cea5fa5ebf632
Thomas Ringot <contact@thomasringot.com>
2018-04-16 13:25:34 +0200

    Refactor ListCard Component

--- components/ListCard/index.js
+++ components/ListCard/index.js
@@ -1,15 +1,10 @@
 import { categoryColors } from "../../constantes/colors";
 
 const ListCard = props => (
-  <div className="row">
-    <div className="col s12 m5">
-      <div className="card-panel teal">
-        <span className="white-text number">{props.children}</span>
-        <br />
-        <span className="white-text number2">{props.children}</span>
-      </div>
-    </div>
-
+  <div className="card-panel teal">
+    <span className="white-text number">{props.children}</span>
+    <br />
+    <span className="white-text number2">{props.children}</span>
     <style jsx>
       {`
         .card-panel {
