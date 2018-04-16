46ac80be5c2c5bc2f0eb6ca958f7f1d4015a492a
Gutsover <mylenevandaele@hmail.com>
2018-04-16 11:38:07 +0100

    Add span 2017 index

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,64 @@
 import Layout from "../components/Layout";
 import Head from "next/head";
+import { sideNavColors } from "../constantes/colors";
 
 export default () => (
   <Layout>
-    <h1 className="main-title">rapport annuel d'activités</h1>
+    <h1 className="main-title"> rapport annuel d 'activités</h1>
+    <span className="year"> 2017 </span>
 
     <style jsx>
       {`
         .main-title {
           font-size: 4rem;
           font-variant: small-caps;
-        }
-
-        .main-title:after {
-          width: 12rem;
-          height: 0.8rem;
-          content: "";
-          display: inline-block;
-          position: relative;
-          background-color: grey;
+          text-align: center;
         }
 
         .main-title:before {
-          width: 12rem;
-          height: 0.8rem;
+          background: -webkit-linear-gradient(
+            left,
+            ${sideNavColors.pink} 0%,
+            ${sideNavColors.purple} 30%,
+            ${sideNavColors.green} 60%,
+            ${sideNavColors.orange} 100%
+          );
           content: "";
-          display: inline-block;
+          display: block;
+          height: 0.8rem;
+          margin-bottom: 1rem;
+          margin-left: 10%;
           position: relative;
-          background-color: grey;
+          width: 40%;
         }
-    `}</style>
+
+        .main-title:after {
+          background: -webkit-linear-gradient(
+            right,
+            ${sideNavColors.pink} 0%,
+            ${sideNavColors.purple} 30%,
+            ${sideNavColors.green} 60%,
+            ${sideNavColors.orange} 100%
+          );
+          content: "";
+          display: block;
+          height: 0.8rem;
+          margin-left: 60%;
+          margin-top: 1rem;
+          position: relative;
+          width: 40%;
+        }
+
+        .year {
+          background-color: Whitesmoke;
+          background-opacity: 0.5;
+          border: solid 5px White;
+          font-size: 5rem;
+          font-weight: bold;
+          margin-left: 60%;
+          padding: 1.5rem;
+        }
+      `}
+    </style>
   </Layout>
 );
