905de6f7f7bc5bf56eb134af2b21dd93e36190b7
Thomas Ringot <contact@thomasringot.com>
2018-04-17 16:15:12 +0200

    Add boilerplate

---
+++ services/NewsService/index.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import firestore from "firebase/firestore";
+
+export default class NewsService {
+  constructor() {
+    console.log("coucou");
+  }
+
+  getNews() {
+    // on va chercher les infos dans la bdd
+    // return les infoos
+  }
+}
