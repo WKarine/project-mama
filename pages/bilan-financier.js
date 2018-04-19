fff77c1156dbda3c9af9ef5b90ce02349fe10bbc
WKarine <karine.wloch@gmail.com>
2018-04-19 23:40:30 +0200

    Add bilan financier

--- pages/bilan-financier.js
+++ pages/bilan-financier.js
@@ -1,5 +1,6 @@
 import Layout from "../components/Layout";
 import H1 from "../components/H1";
+import H2 from "../components/H2";
 
 export default () => (
   <Layout>
@@ -7,6 +8,8 @@ export default () => (
       <div className="row">
         <H1 className="main-title">bilan financier</H1>
       </div>
+
+      <H2>faire une page 404</H2>
     </div>
 
     <style jsx>{``}</style>
