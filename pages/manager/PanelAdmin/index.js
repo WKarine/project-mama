ad4c90437261c7ef01585f6ca4a157d1149f0b6f
Simon Tesniere <simon.tesniere@gmail.com>
2018-04-19 13:33:44 +0200

    Add ConnextioForm and PanelAdmin

--- pages/manager/PanelAdmin/index.js
+++ pages/manager/PanelAdmin/index.js
@@ -1,6 +1,23 @@
+import Link from "next/link";
+
 const PanelAdmin = () => (
   <div>
     <h2>Panel Admin</h2>
+    <ul>
+      <li>
+        <Link href="">
+          <a class="waves-effect waves-light btn">button</a>
+        </Link>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <Link href="">
+          <a class="waves-effect waves-light btn">button</a>
+        </Link>
+      </li>
+    </ul>
+    <style jsx>{``}</style>
   </div>
 );
 
