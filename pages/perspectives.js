21445203ded4f2e64b7d59dda11e1d6f53f98d58
Gutsover <mylenevandaele@hmail.com>
2018-04-20 14:26:32 +0100

    Add css page perspectives

--- pages/perspectives.js
+++ pages/perspectives.js
@@ -9,22 +9,22 @@ export default () => (
       <div className="row">
         <H1>Perspectives</H1>
 
-        <div className="col s12 m4">
-          <figure>
-            <img
-              className="responsive-img photo-director"
-              src="static/images/perspectives/hardy.png"
-              title="director"
-            />
-
-            <figcaption>
-              <strong>"Louis-Marie Hardy,</strong>
-              <br />Directeur Général Pôle Santé Travail"
-            </figcaption>
-          </figure>
+        <div className="col s12 m6">
+          <div className="card">
+            <div className="card-image">
+              <img
+                className="responsive-img photo-director"
+                src="static/images/perspectives/hardy.png"
+                alt="director"
+              />
+              <span className="card-title legend">
+                Louis-Marie Hardy, <br />Directeur général de Pôle Santé Travail
+              </span>
+            </div>
+          </div>
         </div>
 
-        <div className="col s12 m6 offset-m2">
+        <div className="col s12 m6">
           <p>
             Depuis 2011, la santé au travail évolue pour s’adapter au monde de
             l’entreprise, aux salariés et aux ressources des Services de Santé
@@ -46,7 +46,9 @@ export default () => (
             permettant ainsi au médecin du travail de coordonner des actions
             collectives dans une démarche globale de prévention.
           </p>
+        </div>
 
+        <div className="col s12 m6">
           <p>
             Une nouvelle loi qui donne du sens à l’action en entreprise,
             valorise le conseil et la proximité avec les entreprises et les
@@ -57,147 +59,162 @@ export default () => (
             pour prévenir la désinsertion professionnelle.
           </p>
         </div>
-
-        <div className="col s12 m6">
-          <p>
-            La loi a été publiée le 8 août 2016 sous l’appellation LOI n°
-            2016-1088 relative au travail, à la modernisation du dialogue social
-            et à la sécurisation des parcours professionnels (article 102). Le
-            décret d’application a été publié le 27 décembre 2016. L’année 2017
-            sera consacrée à sa mise en application.
-          </p>
-
-          <p>
-            PÔLE SANTÉ TRAVAIL a anticipé ces avancées et l’année 2016 a été
-            marquée par:
-          </p>
-
-          <strong>
-            UNE ORGANISATION CENTRÉE AUTOUR DE L’ÉQUIPE SANTÉ TRAVAIL
-          </strong>
-
-          <p>
-            La santé au travail est un travail d’équipe et le déploiement de
-            notre organisation s’est poursuivi. Plus de 50% des professionnels
-            de terrain rentrent aujourd’hui dans ce dispositif soit 56 équipes
-            sur l’ensemble du territoire de PÔLE SANTÉ TRAVAIL.
-          </p>
-
-          <strong>
-            UNE POLITIQUE PATRIMONIALE AMBITIEUSE QUI FAVORISE UN SUIVI DE SANTÉ
-            DE QUALITÉ
-          </strong>
-
-          <p>
-            PÔLE SANTÉ TRAVAIL poursuit un double objectif à travers la mise en
-            place de sa politique patrimoniale : offrir aux équipes de terrain
-            un outil de travail adapté et permettre aux salariés des entreprises
-            adhérentes de recevoir le meilleur suivi de santé possible. En 2016,
-            les travaux de rénovation ou d’agrandissement ont concerné
-            Hallennes-lez- Haubourdin et Leers. Le centre de Lille Jeanne d’Arc
-            a subi de grandes transformations pour accueillir les équipes de
-            Lille Masséna.
-          </p>
-
-          <p>
-            Le maillage territorial s’est affiné et en 2016, nous comptons 24
-            centres de santé au travail.
-          </p>
+        <div className="row">
+          <div className="col s12 m6 offset-m3">
+            <p>
+              La loi a été publiée le 8 août 2016 sous l’appellation LOI n°
+              2016-1088 relative au travail, à la modernisation du dialogue
+              social et à la sécurisation des parcours professionnels (article
+              102). Le décret d’application a été publié le 27 décembre 2016.
+              L’année 2017 sera consacrée à sa mise en application.
+            </p>
+            <p>
+              PÔLE SANTÉ TRAVAIL a anticipé ces avancées et l’année 2016 a été
+              marquée par:
+            </p>
+            <strong>
+              UNE ORGANISATION CENTRÉE AUTOUR DE L’ÉQUIPE SANTÉ TRAVAIL
+            </strong>
+          </div>
         </div>
-      </div>
 
-      <div className="col s12 m5">
-        <strong>
-          UNE MEILLEURE TRAÇABILITÉ ET UNE VALORISATION DE NOS ACTIONS AUTOUR
-          D’UN PROJET DIGITAL : SANTÉ TRAVAIL 3.0
-        </strong>
+        <div className="row">
+          <div className="col s12 m6 offset-m5">
+            <p>
+              La santé au travail est un travail d’équipe et le déploiement de
+              notre organisation s’est poursuivi. Plus de 50% des professionnels
+              de terrain rentrent aujourd’hui dans ce dispositif soit 56 équipes
+              sur l’ensemble du territoire de PÔLE SANTÉ TRAVAIL.
+            </p>
+            <strong>
+              UNE POLITIQUE PATRIMONIALE AMBITIEUSE QUI FAVORISE UN SUIVI DE
+              SANTÉ DE QUALITÉ
+            </strong>
+            <p>
+              PÔLE SANTÉ TRAVAIL poursuit un double objectif à travers la mise
+              en place de sa politique patrimoniale : offrir aux équipes de
+              terrain un outil de travail adapté et permettre aux salariés des
+              entreprises adhérentes de recevoir le meilleur suivi de santé
+              possible. En 2016, les travaux de rénovation ou d’agrandissement
+              ont concerné Hallennes-lez- Haubourdin et Leers. Le centre de
+              Lille Jeanne d’Arc a subi de grandes transformations pour
+              accueillir les équipes de Lille Masséna.
+            </p>
+          </div>
+        </div>
 
-        <p>
-          Santé travail 3.0 recouvre toutes les dimensions de notre métier. Dans
-          ce projet global, plusieurs outils sont développés :
-        </p>
+        <div className="row">
+          <div className="col s12 m12">
+            <p>
+              Le maillage territorial s’est affiné et en 2016, nous comptons 24
+              centres de santé au travail.
+            </p>
+          </div>
+          <div className="col s12 m6 offset-m3">
+            <strong>
+              UNE MEILLEURE TRAÇABILITÉ ET UNE VALORISATION DE NOS ACTIONS
+              AUTOUR D’UN PROJET DIGITAL : SANTÉ TRAVAIL 3.0
+            </strong>
+          </div>
+          <div className="col s12 m12">
+            <p>
+              Santé travail 3.0 recouvre toutes les dimensions de notre métier.
+              Dans ce projet global, plusieurs outils sont développés :
+            </p>
+            <p>
+              Pour les entreprises adhérentes, le portail web a été complètement
+              revu. Des groupes utilisateurs internes et externes ont pu tester
+              les différentes fonctionnalités et améliorer l’outil. Le premier
+              lot s’est concentré à améliorer l’expérience utilisateur sur les
+              obligations de déclaration mais notre souhait est de proposer une
+              interface relationnelle qui va au-delà du volet administratif et
+              comptable et mieux identifier tous nos contacts en entreprises. En
+              parallèle l’objectif est de mettre en lisibilité aussi nos actions
+              en entreprise.
+            </p>
+          </div>
+        </div>
 
-        <p>
-          Pour les entreprises adhérentes, le portail web a été complètement
-          revu. Des groupes utilisateurs internes et externes ont pu tester les
-          différentes fonctionnalités et améliorer l’outil. Le premier lot s’est
-          concentré à améliorer l’expérience utilisateur sur les obligations de
-          déclaration mais notre souhait est de proposer une interface
-          relationnelle qui va au-delà du volet administratif et comptable et
-          mieux identifier tous nos contacts en entreprises. En parallèle
-          l’objectif est de mettre en lisibilité aussi nos actions en
-          entreprise.
-        </p>
+        <div className="row">
+          <div className="col s12 m6 offset-m5">
+            <p>
+              Pour les professionnels de santé au travail, le logiciel métier
+              est en cours de refonte. Un cahier des charges a été rédigé afin
+              d’adapter les outils aux nouvelles pratiques et aux enjeux de
+              traçabilité des actions.
+            </p>
 
-        <p>
-          Pour les professionnels de santé au travail, le logiciel métier est en
-          cours de refonte. Un cahier des charges a été rédigé afin d’adapter
-          les outils aux nouvelles pratiques et aux enjeux de traçabilité des
-          actions.
-        </p>
+            <p>
+              Pour tous les salariés de PÔLE SANTÉ TRAVAIL, un intranet
+              collaboratif a été mis en place pour favoriser la circulation de
+              l’information et promouvoir le travail collaboratif. Pour les
+              services de santé au travail, PÔLE SANTÉ TRAVAIL a souhaité
+              initier un partenariat unique en France avec 15 services de santé
+              au travail pour la création d’une base de données en toxicologie
+              qui permettra une meilleure identification des substances CMR et
+              donc une action sur la substitution des produits.
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12 m5 offset-m4">
+            <strong>"UNE COMMUNICATION EXTERNE ACTIVE"</strong>
+          </div>
+        </div>
 
-        <p>
-          Pour tous les salariés de PÔLE SANTÉ TRAVAIL, un intranet collaboratif
-          a été mis en place pour favoriser la circulation de l’information et
-          promouvoir le travail collaboratif. Pour les services de santé au
-          travail, PÔLE SANTÉ TRAVAIL a souhaité initier un partenariat unique
-          en France avec 15 services de santé au travail pour la création d’une
-          base de données en toxicologie qui permettra une meilleure
-          identification des substances CMR et donc une action sur la
-          substitution des produits.
-        </p>
-      </div>
+        <div className="row">
+          <div className="col s12 m12">
+            <p>
+              La politique de valorisation et de promotion de la santé au
+              travail se poursuit. Le point fort de cette année 2016 a été
+              l’organisation du salon PRÉVENTICA : pendant 3 jours conférences,
+              ateliers, stand ont permis de donner de la lisibilité sur les
+              missions du service de santé au travail. Des grands rendez-vous
+              comme le salon des créateurs d’entreprises et des TPE « CREER » et
+              les rencontres Experts-comptables ont été renouvelés. Dans le
+              cadre de notre communication autour du nouvel espace adhérent, des
+              testings utilisateurs ont été organisés. Notre participation à la
+              1ère édition du salon des entreprises a été l’occasion de
+              promouvoirce nouvel outil. L’assemblée générale 2016 a proposé des
+              ateliers autour de la prévention des addictions, du maintien dans
+              l’emploi et du juridique. 168 supports d’informations ont été
+              réalisés sur l’année.
+            </p>
+          </div>
 
-      <div className="col s12 m5">
-        <strong>UNE COMMUNICATION EXTERNE ACTIVE</strong>
+          <div className="col s12 m5 offset-m4">
+            <strong>
+              "UNE POLITIQUE DE FORMATION ET DE RECRUTEMENT VOLONTARISTE"
+            </strong>
+          </div>
 
-        <p>
-          La politique de valorisation et de promotion de la santé au travail se
-          poursuit. Le point fort de cette année 2016 a été l’organisation du
-          salon PRÉVENTICA : pendant 3 jours conférences, ateliers, stand ont
-          permis de donner de la lisibilité sur les missions du service de santé
-          au travail. Des grands rendez-vous comme le salon des créateurs
-          d’entreprises et des TPE « CREER » et les rencontres
-          Experts-comptables ont été renouvelés. Dans le cadre de notre
-          communication autour du nouvel espace adhérent, des testings
-          utilisateurs ont été organisés. Notre participation à la 1ère édition
-          du salon des entreprises a été l’occasion de promouvoirce nouvel
-          outil. L’assemblée générale 2016 a proposé des ateliers autour de la
-          prévention des addictions, du maintien dans l’emploi et du juridique.
-          168 supports d’informations ont été réalisés sur l’année.
-        </p>
-
-        <strong>
-          UNE POLITIQUE DE FORMATION ET DE RECRUTEMENT VOLONTARISTE
-        </strong>
-
-        <p>
-          Pour PÔLE SANTÉ TRAVAIL, la formation des salariés est un outil
-          indispensable d’adaptation à l’évolution des missions et des métiers
-          qui permet le développement des compétences. Un plan de formation
-          ambitieux apporte une forte valeur ajoutée dans les prestations
-          proposées en réponse aux besoins actuels des entreprises. 6,3 % de la
-          masse salariale sont investis dans la formation. La Direction des
-          Ressources Humaines s’implique également dans des événements comme le
-          congrès des internes en santé travail pour nouer des liens privilégiés
-          avec les médecins du travail de demain.
-        </p>
+          <div className="col s12 m8 offset-m2">
+            <p>
+              Pour PÔLE SANTÉ TRAVAIL, la formation des salariés est un outil
+              indispensable d’adaptation à l’évolution des missions et des
+              métiers qui permet le développement des compétences. Un plan de
+              formation ambitieux apporte une forte valeur ajoutée dans les
+              prestations proposées en réponse aux besoins actuels des
+              entreprises. 6,3 % de la masse salariale sont investis dans la
+              formation. La Direction des Ressources Humaines s’implique
+              également dans des événements comme le congrès des internes en
+              santé travail pour nouer des liens privilégiés avec les médecins
+              du travail de demain.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
 
-    <style jsx>{`
-      .photo-director {
-        border: 2px inset ${categoryColors.blue};
-        border-radius: 50%;
-        padding: 0;
-        margin-right: 1rem;
+    <style jsx global>{`
+      .container-fluid {
+        background-color: #ffffff;
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='192' height='192' viewBox='0 0 192 192'%3E%3Cpath fill='%2322c0f9' fill-opacity='0.06' d='M192 15v2a11 11 0 0 0-11 11c0 1.94 1.16 4.75 2.53 6.11l2.36 2.36a6.93 6.93 0 0 1 1.22 7.56l-.43.84a8.08 8.08 0 0 1-6.66 4.13H145v35.02a6.1 6.1 0 0 0 3.03 4.87l.84.43c1.58.79 4 .4 5.24-.85l2.36-2.36a12.04 12.04 0 0 1 7.51-3.11 13 13 0 1 1 .02 26 12 12 0 0 1-7.53-3.11l-2.36-2.36a4.93 4.93 0 0 0-5.24-.85l-.84.43a6.1 6.1 0 0 0-3.03 4.87V143h35.02a8.08 8.08 0 0 1 6.66 4.13l.43.84a6.91 6.91 0 0 1-1.22 7.56l-2.36 2.36A10.06 10.06 0 0 0 181 164a11 11 0 0 0 11 11v2a13 13 0 0 1-13-13 12 12 0 0 1 3.11-7.53l2.36-2.36a4.93 4.93 0 0 0 .85-5.24l-.43-.84a6.1 6.1 0 0 0-4.87-3.03H145v35.02a8.08 8.08 0 0 1-4.13 6.66l-.84.43a6.91 6.91 0 0 1-7.56-1.22l-2.36-2.36A10.06 10.06 0 0 0 124 181a11 11 0 0 0-11 11h-2a13 13 0 0 1 13-13c2.47 0 5.79 1.37 7.53 3.11l2.36 2.36a4.94 4.94 0 0 0 5.24.85l.84-.43a6.1 6.1 0 0 0 3.03-4.87V145h-35.02a8.08 8.08 0 0 1-6.66-4.13l-.43-.84a6.91 6.91 0 0 1 1.22-7.56l2.36-2.36A10.06 10.06 0 0 0 107 124a11 11 0 0 0-22 0c0 1.94 1.16 4.75 2.53 6.11l2.36 2.36a6.93 6.93 0 0 1 1.22 7.56l-.43.84a8.08 8.08 0 0 1-6.66 4.13H49v35.02a6.1 6.1 0 0 0 3.03 4.87l.84.43c1.58.79 4 .4 5.24-.85l2.36-2.36a12.04 12.04 0 0 1 7.51-3.11A13 13 0 0 1 81 192h-2a11 11 0 0 0-11-11c-1.94 0-4.75 1.16-6.11 2.53l-2.36 2.36a6.93 6.93 0 0 1-7.56 1.22l-.84-.43a8.08 8.08 0 0 1-4.13-6.66V145H11.98a6.1 6.1 0 0 0-4.87 3.03l-.43.84c-.79 1.58-.4 4 .85 5.24l2.36 2.36a12.04 12.04 0 0 1 3.11 7.51A13 13 0 0 1 0 177v-2a11 11 0 0 0 11-11c0-1.94-1.16-4.75-2.53-6.11l-2.36-2.36a6.93 6.93 0 0 1-1.22-7.56l.43-.84a8.08 8.08 0 0 1 6.66-4.13H47v-35.02a6.1 6.1 0 0 0-3.03-4.87l-.84-.43c-1.59-.8-4-.4-5.24.85l-2.36 2.36A12 12 0 0 1 28 109a13 13 0 1 1 0-26c2.47 0 5.79 1.37 7.53 3.11l2.36 2.36a4.94 4.94 0 0 0 5.24.85l.84-.43A6.1 6.1 0 0 0 47 84.02V49H11.98a8.08 8.08 0 0 1-6.66-4.13l-.43-.84a6.91 6.91 0 0 1 1.22-7.56l2.36-2.36A10.06 10.06 0 0 0 11 28 11 11 0 0 0 0 17v-2a13 13 0 0 1 13 13c0 2.47-1.37 5.79-3.11 7.53l-2.36 2.36a4.94 4.94 0 0 0-.85 5.24l.43.84A6.1 6.1 0 0 0 11.98 47H47V11.98a8.08 8.08 0 0 1 4.13-6.66l.84-.43a6.91 6.91 0 0 1 7.56 1.22l2.36 2.36A10.06 10.06 0 0 0 68 11 11 11 0 0 0 79 0h2a13 13 0 0 1-13 13 12 12 0 0 1-7.53-3.11l-2.36-2.36a4.93 4.93 0 0 0-5.24-.85l-.84.43A6.1 6.1 0 0 0 49 11.98V47h35.02a8.08 8.08 0 0 1 6.66 4.13l.43.84a6.91 6.91 0 0 1-1.22 7.56l-2.36 2.36A10.06 10.06 0 0 0 85 68a11 11 0 0 0 22 0c0-1.94-1.16-4.75-2.53-6.11l-2.36-2.36a6.93 6.93 0 0 1-1.22-7.56l.43-.84a8.08 8.08 0 0 1 6.66-4.13H143V11.98a6.1 6.1 0 0 0-3.03-4.87l-.84-.43c-1.59-.8-4-.4-5.24.85l-2.36 2.36A12 12 0 0 1 124 13a13 13 0 0 1-13-13h2a11 11 0 0 0 11 11c1.94 0 4.75-1.16 6.11-2.53l2.36-2.36a6.93 6.93 0 0 1 7.56-1.22l.84.43a8.08 8.08 0 0 1 4.13 6.66V47h35.02a6.1 6.1 0 0 0 4.87-3.03l.43-.84c.8-1.59.4-4-.85-5.24l-2.36-2.36A12 12 0 0 1 179 28a13 13 0 0 1 13-13zM84.02 143a6.1 6.1 0 0 0 4.87-3.03l.43-.84c.8-1.59.4-4-.85-5.24l-2.36-2.36A12 12 0 0 1 83 124a13 13 0 1 1 26 0c0 2.47-1.37 5.79-3.11 7.53l-2.36 2.36a4.94 4.94 0 0 0-.85 5.24l.43.84a6.1 6.1 0 0 0 4.87 3.03H143v-35.02a8.08 8.08 0 0 1 4.13-6.66l.84-.43a6.91 6.91 0 0 1 7.56 1.22l2.36 2.36A10.06 10.06 0 0 0 164 107a11 11 0 0 0 0-22c-1.94 0-4.75 1.16-6.11 2.53l-2.36 2.36a6.93 6.93 0 0 1-7.56 1.22l-.84-.43a8.08 8.08 0 0 1-4.13-6.66V49h-35.02a6.1 6.1 0 0 0-4.87 3.03l-.43.84c-.79 1.58-.4 4 .85 5.24l2.36 2.36a12.04 12.04 0 0 1 3.11 7.51A13 13 0 1 1 83 68a12 12 0 0 1 3.11-7.53l2.36-2.36a4.93 4.93 0 0 0 .85-5.24l-.43-.84A6.1 6.1 0 0 0 84.02 49H49v35.02a8.08 8.08 0 0 1-4.13 6.66l-.84.43a6.91 6.91 0 0 1-7.56-1.22l-2.36-2.36A10.06 10.06 0 0 0 28 85a11 11 0 0 0 0 22c1.94 0 4.75-1.16 6.11-2.53l2.36-2.36a6.93 6.93 0 0 1 7.56-1.22l.84.43a8.08 8.08 0 0 1 4.13 6.66V143h35.02z'%3E%3C/path%3E%3C/svg%3E");
       }
 
-      figcaption {
-        color: ${categoryColors.blue};
-        font-size: 1rem;
-        font-style: italic;
-        text-align: center;
+      .photo-director {
+        padding: 0;
+        margin-right: 1rem;
       }
 
       p {
@@ -206,6 +223,7 @@ export default () => (
 
       strong {
         color: ${categoryColors.blue};
+        font-size: 1.4rem;
         font-weight: bold;
       }
     `}</style>
