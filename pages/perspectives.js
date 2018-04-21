5d67e6151ed37c84f80da1b744d2fc8da58772ce
Thomas Ringot <contact@thomasringot.com>
2018-04-21 21:17:49 +0200

    Add styling

--- pages/perspectives.js
+++ pages/perspectives.js
@@ -7,10 +7,10 @@ export default () => (
   <Layout>
     <div className="container">
       <div className="row">
-        <H1>Perspectives</H1>
+        <H1>perspectives</H1>
 
-        <div className="col s12 m6">
-          <div className="card">
+        <div className="col s12 valign">
+          <div className="card valign-left">
             <div className="card-image">
               <img
                 className="responsive-img photo-director"
@@ -22,24 +22,26 @@ export default () => (
               </span>
             </div>
           </div>
+
+          <div className="valign-right">
+            <p>
+              Depuis 2011, la santé au travail évolue pour s’adapter au monde de
+              l’entreprise, aux salariés et aux ressources des Services de Santé
+              au Travail Inter-entreprises.
+            </p>
+
+            <p>
+              2016 est à ce titre une année charnière et PÔLE SANTÉ TRAVAIL
+              s’est investi sur le plan national pour peser dans les débats et
+              contribuer à l’écriture d’une nouvelle loi, qui s’écarte des
+              systématismes pour permettre un suivi de santé adapté aux métiers,
+              aux conditions de travail, à l’âge et aux situations personnelles
+              des salariés.
+            </p>
+          </div>
         </div>
 
-        <div className="col s12 m6">
-          <p>
-            Depuis 2011, la santé au travail évolue pour s’adapter au monde de
-            l’entreprise, aux salariés et aux ressources des Services de Santé
-            au Travail Inter-entreprises.
-          </p>
-
-          <p>
-            2016 est à ce titre une année charnière et PÔLE SANTÉ TRAVAIL s’est
-            investi sur le plan national pour peser dans les débats et
-            contribuer à l’écriture d’une nouvelle loi, qui s’écarte des
-            systématismes pour permettre un suivi de santé adapté aux métiers,
-            aux conditions de travail, à l’âge et aux situations personnelles
-            des salariés.
-          </p>
-
+        <div className="col s12">
           <p>
             Une nouvelle loi qui replace la prévention au cœur de nos métiers
             avec une diminution des obligations liées au suivi de santé,
@@ -48,17 +50,22 @@ export default () => (
           </p>
         </div>
 
-        <div className="col s12 m6">
-          <p>
-            Une nouvelle loi qui donne du sens à l’action en entreprise,
-            valorise le conseil et la proximité avec les entreprises et les
-            branches professionnelles. Une nouvelle loi qui permette aussi au
-            salarié de comprendre qu’il a une place majeure dans ce dispositif
-            et que le service de santé au travail est présent tout au long de
-            son parcours professionnel pour l’accompagner dans ses évolutions et
-            pour prévenir la désinsertion professionnelle.
-          </p>
+        <div className="col s12 valign">
+          <div className="valign-left">
+            <p>
+              Une nouvelle loi qui donne du sens à l’action en entreprise,
+              valorise le conseil et la proximité avec les entreprises et les
+              branches professionnelles. Une nouvelle loi qui permette aussi au
+              salarié de comprendre qu’il a une place majeure dans ce dispositif
+              et que le service de santé au travail est présent tout au long de
+              son parcours professionnel pour l’accompagner dans ses évolutions
+              et pour prévenir la désinsertion professionnelle.
+            </p>
+          </div>
+
+          <i className="valign-right large material-icons">account_balance</i>
         </div>
+
         <div className="row">
           <div className="col s12 m6 offset-m3">
             <p>
@@ -79,18 +86,27 @@ export default () => (
         </div>
 
         <div className="row">
-          <div className="col s12 m6 offset-m5">
-            <p>
+          <div className="col s12 valign">
+            <i className="valign-left large material-icons">
+              supervisor_account
+            </i>
+            <p className="valign-right">
               La santé au travail est un travail d’équipe et le déploiement de
               notre organisation s’est poursuivi. Plus de 50% des professionnels
               de terrain rentrent aujourd’hui dans ce dispositif soit 56 équipes
               sur l’ensemble du territoire de PÔLE SANTÉ TRAVAIL.
             </p>
+          </div>
+
+          <div className="col s12 m8 offset-m2">
             <strong>
               UNE POLITIQUE PATRIMONIALE AMBITIEUSE QUI FAVORISE UN SUIVI DE
               SANTÉ DE QUALITÉ
             </strong>
-            <p>
+          </div>
+
+          <div className="col s12 valign">
+            <p className="valign-left">
               PÔLE SANTÉ TRAVAIL poursuit un double objectif à travers la mise
               en place de sa politique patrimoniale : offrir aux équipes de
               terrain un outil de travail adapté et permettre aux salariés des
@@ -100,6 +116,7 @@ export default () => (
               Lille Jeanne d’Arc a subi de grandes transformations pour
               accueillir les équipes de Lille Masséna.
             </p>
+            <i className="valign-right large material-icons">adjust</i>
           </div>
         </div>
 
@@ -110,7 +127,7 @@ export default () => (
               centres de santé au travail.
             </p>
           </div>
-          <div className="col s12 m6 offset-m3">
+          <div className="col s12 m8 offset-m2">
             <strong>
               UNE MEILLEURE TRAÇABILITÉ ET UNE VALORISATION DE NOS ACTIONS
               AUTOUR D’UN PROJET DIGITAL : SANTÉ TRAVAIL 3.0
@@ -136,15 +153,14 @@ export default () => (
         </div>
 
         <div className="row">
-          <div className="col s12 m6 offset-m5">
-            <p>
+          <div className="col s12 valign">
+            <i className="valign-left large material-icons">apps</i>
+            <p className="valign-right">
               Pour les professionnels de santé au travail, le logiciel métier
               est en cours de refonte. Un cahier des charges a été rédigé afin
               d’adapter les outils aux nouvelles pratiques et aux enjeux de
               traçabilité des actions.
-            </p>
-
-            <p>
+              <br />
               Pour tous les salariés de PÔLE SANTÉ TRAVAIL, un intranet
               collaboratif a été mis en place pour favoriser la circulation de
               l’information et promouvoir le travail collaboratif. Pour les
@@ -182,7 +198,7 @@ export default () => (
             </p>
           </div>
 
-          <div className="col s12 m5 offset-m4">
+          <div className="col s12 m8 offset-m2">
             <strong>
               "UNE POLITIQUE DE FORMATION ET DE RECRUTEMENT VOLONTARISTE"
             </strong>
@@ -206,15 +222,15 @@ export default () => (
       </div>
     </div>
 
-    <style jsx global>{`
-      .container-fluid {
-        background-color: #ffffff;
-        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='192' height='192' viewBox='0 0 192 192'%3E%3Cpath fill='%2322c0f9' fill-opacity='0.06' d='M192 15v2a11 11 0 0 0-11 11c0 1.94 1.16 4.75 2.53 6.11l2.36 2.36a6.93 6.93 0 0 1 1.22 7.56l-.43.84a8.08 8.08 0 0 1-6.66 4.13H145v35.02a6.1 6.1 0 0 0 3.03 4.87l.84.43c1.58.79 4 .4 5.24-.85l2.36-2.36a12.04 12.04 0 0 1 7.51-3.11 13 13 0 1 1 .02 26 12 12 0 0 1-7.53-3.11l-2.36-2.36a4.93 4.93 0 0 0-5.24-.85l-.84.43a6.1 6.1 0 0 0-3.03 4.87V143h35.02a8.08 8.08 0 0 1 6.66 4.13l.43.84a6.91 6.91 0 0 1-1.22 7.56l-2.36 2.36A10.06 10.06 0 0 0 181 164a11 11 0 0 0 11 11v2a13 13 0 0 1-13-13 12 12 0 0 1 3.11-7.53l2.36-2.36a4.93 4.93 0 0 0 .85-5.24l-.43-.84a6.1 6.1 0 0 0-4.87-3.03H145v35.02a8.08 8.08 0 0 1-4.13 6.66l-.84.43a6.91 6.91 0 0 1-7.56-1.22l-2.36-2.36A10.06 10.06 0 0 0 124 181a11 11 0 0 0-11 11h-2a13 13 0 0 1 13-13c2.47 0 5.79 1.37 7.53 3.11l2.36 2.36a4.94 4.94 0 0 0 5.24.85l.84-.43a6.1 6.1 0 0 0 3.03-4.87V145h-35.02a8.08 8.08 0 0 1-6.66-4.13l-.43-.84a6.91 6.91 0 0 1 1.22-7.56l2.36-2.36A10.06 10.06 0 0 0 107 124a11 11 0 0 0-22 0c0 1.94 1.16 4.75 2.53 6.11l2.36 2.36a6.93 6.93 0 0 1 1.22 7.56l-.43.84a8.08 8.08 0 0 1-6.66 4.13H49v35.02a6.1 6.1 0 0 0 3.03 4.87l.84.43c1.58.79 4 .4 5.24-.85l2.36-2.36a12.04 12.04 0 0 1 7.51-3.11A13 13 0 0 1 81 192h-2a11 11 0 0 0-11-11c-1.94 0-4.75 1.16-6.11 2.53l-2.36 2.36a6.93 6.93 0 0 1-7.56 1.22l-.84-.43a8.08 8.08 0 0 1-4.13-6.66V145H11.98a6.1 6.1 0 0 0-4.87 3.03l-.43.84c-.79 1.58-.4 4 .85 5.24l2.36 2.36a12.04 12.04 0 0 1 3.11 7.51A13 13 0 0 1 0 177v-2a11 11 0 0 0 11-11c0-1.94-1.16-4.75-2.53-6.11l-2.36-2.36a6.93 6.93 0 0 1-1.22-7.56l.43-.84a8.08 8.08 0 0 1 6.66-4.13H47v-35.02a6.1 6.1 0 0 0-3.03-4.87l-.84-.43c-1.59-.8-4-.4-5.24.85l-2.36 2.36A12 12 0 0 1 28 109a13 13 0 1 1 0-26c2.47 0 5.79 1.37 7.53 3.11l2.36 2.36a4.94 4.94 0 0 0 5.24.85l.84-.43A6.1 6.1 0 0 0 47 84.02V49H11.98a8.08 8.08 0 0 1-6.66-4.13l-.43-.84a6.91 6.91 0 0 1 1.22-7.56l2.36-2.36A10.06 10.06 0 0 0 11 28 11 11 0 0 0 0 17v-2a13 13 0 0 1 13 13c0 2.47-1.37 5.79-3.11 7.53l-2.36 2.36a4.94 4.94 0 0 0-.85 5.24l.43.84A6.1 6.1 0 0 0 11.98 47H47V11.98a8.08 8.08 0 0 1 4.13-6.66l.84-.43a6.91 6.91 0 0 1 7.56 1.22l2.36 2.36A10.06 10.06 0 0 0 68 11 11 11 0 0 0 79 0h2a13 13 0 0 1-13 13 12 12 0 0 1-7.53-3.11l-2.36-2.36a4.93 4.93 0 0 0-5.24-.85l-.84.43A6.1 6.1 0 0 0 49 11.98V47h35.02a8.08 8.08 0 0 1 6.66 4.13l.43.84a6.91 6.91 0 0 1-1.22 7.56l-2.36 2.36A10.06 10.06 0 0 0 85 68a11 11 0 0 0 22 0c0-1.94-1.16-4.75-2.53-6.11l-2.36-2.36a6.93 6.93 0 0 1-1.22-7.56l.43-.84a8.08 8.08 0 0 1 6.66-4.13H143V11.98a6.1 6.1 0 0 0-3.03-4.87l-.84-.43c-1.59-.8-4-.4-5.24.85l-2.36 2.36A12 12 0 0 1 124 13a13 13 0 0 1-13-13h2a11 11 0 0 0 11 11c1.94 0 4.75-1.16 6.11-2.53l2.36-2.36a6.93 6.93 0 0 1 7.56-1.22l.84.43a8.08 8.08 0 0 1 4.13 6.66V47h35.02a6.1 6.1 0 0 0 4.87-3.03l.43-.84c.8-1.59.4-4-.85-5.24l-2.36-2.36A12 12 0 0 1 179 28a13 13 0 0 1 13-13zM84.02 143a6.1 6.1 0 0 0 4.87-3.03l.43-.84c.8-1.59.4-4-.85-5.24l-2.36-2.36A12 12 0 0 1 83 124a13 13 0 1 1 26 0c0 2.47-1.37 5.79-3.11 7.53l-2.36 2.36a4.94 4.94 0 0 0-.85 5.24l.43.84a6.1 6.1 0 0 0 4.87 3.03H143v-35.02a8.08 8.08 0 0 1 4.13-6.66l.84-.43a6.91 6.91 0 0 1 7.56 1.22l2.36 2.36A10.06 10.06 0 0 0 164 107a11 11 0 0 0 0-22c-1.94 0-4.75 1.16-6.11 2.53l-2.36 2.36a6.93 6.93 0 0 1-7.56 1.22l-.84-.43a8.08 8.08 0 0 1-4.13-6.66V49h-35.02a6.1 6.1 0 0 0-4.87 3.03l-.43.84c-.79 1.58-.4 4 .85 5.24l2.36 2.36a12.04 12.04 0 0 1 3.11 7.51A13 13 0 1 1 83 68a12 12 0 0 1 3.11-7.53l2.36-2.36a4.93 4.93 0 0 0 .85-5.24l-.43-.84A6.1 6.1 0 0 0 84.02 49H49v35.02a8.08 8.08 0 0 1-4.13 6.66l-.84.43a6.91 6.91 0 0 1-7.56-1.22l-2.36-2.36A10.06 10.06 0 0 0 28 85a11 11 0 0 0 0 22c1.94 0 4.75-1.16 6.11-2.53l2.36-2.36a6.93 6.93 0 0 1 7.56-1.22l.84.43a8.08 8.08 0 0 1 4.13 6.66V143h35.02z'%3E%3C/path%3E%3C/svg%3E");
-      }
-
+    <style jsx>{`
       .photo-director {
         padding: 0;
         margin-right: 1rem;
+      }
+
+      .photo-director + .legend {
+        background-color: rgba(0, 0, 0, 0.3);
+        text-align: center;
       }
 
       p {
@@ -225,6 +241,40 @@ export default () => (
         color: ${categoryColors.blue};
         font-size: 1.4rem;
         font-weight: bold;
+        display: block;
+        text-align: center;
+      }
+
+      .valign {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      @media only screen and (max-width: 992px) {
+        .valign {
+          flex-wrap: wrap;
+        }
+      }
+
+      .valign-left,
+      .valign-right {
+        margin: 1rem;
+        flex-basis: 50%;
+      }
+
+      @media only screen and (max-width: 992px) {
+        .valign-left,
+        .valign-right {
+          flex-basis: 100%;
+        }
+      }
+
+      i.valign-left,
+      i.valign-right {
+        display: flex;
+        justify-content: center;
+        align-items: center;
       }
     `}</style>
   </Layout>
