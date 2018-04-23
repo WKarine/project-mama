e8c446d121881321b70b7e79a04486b8f579c182
Thomas Ringot <contact@thomasringot.com>
2018-04-23 14:42:40 +0200

    WIP

--- pages/manager/index.js
+++ pages/manager/index.js
@@ -1,21 +1,55 @@
+import firebase from "firebase/app";
+import auth from "firebase/auth";
+
+import config from "../../constantes/firebase-config";
+
 import Layout from "../../components/Layout";
 
 import H1 from "../../components/H1";
 import ConnectionForm from "./ConnectionForm";
 import PanelAdmin from "./PanelAdmin";
 
-export default () => (
-  <Layout>
-    <div className="container">
-      <div className="row">
-        <H1>Admin</H1>
+class Manager extends React.Component {
+  state = {
+    isConnected: false
+  };
 
-        <div className="col s12">
-          <PanelAdmin />
+  componentDidMount() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    }
+
+    const test = firebase
+      .auth()
+      .signInWithEmailAndPassword("team.mamaf3@gmail.com", "team@mama69")
+      .catch(function(error) {
+        // Rediriger + message erreur
+      });
+
+    console.dir(test);
+  }
+
+  state = {
+    isConnected: true
+  };
+
+  render() {
+    return (
+      <Layout>
+        <div className="container">
+          <div className="row">
+            <H1>Admin</H1>
+
+            <div className="col s12 m8 offset-m2">
+              {this.state.isConnected ? <PanelAdmin /> : <ConnectionForm />}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
 
-    <style jsx>{``}</style>
-  </Layout>
-);
+        <style jsx>{``}</style>
+      </Layout>
+    );
+  }
+}
+
+export default Manager;
